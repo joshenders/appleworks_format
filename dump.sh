25e6eb84d4831b0747ee7bdfcb91de33179282ec
Josh Enders <josh.enders@gmail.com>
2010-02-27 01:08:09 -0800

    initial commit

---
+++ dump.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+files=$(find . -type f -maxdepth 1 | grep -v $0 | sed 's/\.\///g')
+
+if [[ ! -d dumps ]]; then
+  [[ -n "${files}" ]] && \
+    mkdir dumps
+fi
+
+[[ -n "${files}" ]] && \
+  for i in ${files}; do xxd $i > dumps/$i.xxd; done
