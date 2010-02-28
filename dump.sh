12b554ec4992aa9a542b1ffbb6f25319b86bce97
Josh Enders <josh.enders@gmail.com>
2010-02-27 17:29:16 -0800

    cleaned up syntax

--- dump.sh
+++ dump.sh
@@ -3,9 +3,9 @@
 files=$(find . -type f -maxdepth 1 | grep -v $0 | sed 's/\.\///g')
 
 if [[ ! -d dumps ]]; then
-  [[ -n "${files}" ]] && \
-    mkdir dumps
+  [[ -n "${files}" ]] && mkdir dumps
 fi
 
-[[ -n "${files}" ]] && \
+if [[ -n "${files}" ]]; then
   for i in ${files}; do xxd $i > dumps/$i.xxd; done
+fi
